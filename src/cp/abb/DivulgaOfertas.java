f824a59381e90bb84a0497c1f2ee2272a70b1b96
Matheus Gonçalves <matheus.g2003@hotmail.com>
2023-10-15 21:05:55 -0300

    checkpoint finished

--- src/cp/abb/DivulgaOfertas.java
+++ src/cp/abb/DivulgaOfertas.java
@@ -3,6 +3,8 @@ package cp.abb;
 import cp.abb.entities.ABB;
 import cp.abb.entities.Cliente;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DivulgaOfertas {
@@ -15,10 +17,6 @@ public class DivulgaOfertas {
      */
     public static void main(String[] args) {
         Scanner le = new Scanner(System.in);
-        /*
-         * Cria a uma árvore de busca binária para cada tipo de conta
-         * (pessoa física ou jurídica)
-         */
         ABB arvoreFisica = new ABB();
         ABB arvoreJuridica = new ABB();
 
@@ -26,6 +24,7 @@ public class DivulgaOfertas {
         String nome, cpfCnpj;
         String tipoConta = null;
         double saldo;
+
         do {
             System.out.println(" 0 - Encerrar o programa");
             System.out.println(" 1 - Inscrição cliente");
@@ -58,15 +57,11 @@ public class DivulgaOfertas {
                     System.out.print("Informe saldo em aplicações R$: ");
                     saldo = le.nextDouble();
                     Cliente cliente = new Cliente(nome, cpfCnpj, numeroConta, tipoConta, saldo);
-                    if(tipoConta.equals("Física")){
+                    if (tipoConta.equals("Física")) {
                         arvoreFisica.root = arvoreFisica.inserir(arvoreFisica.root, cliente);
-                    }else{
+                    } else {
                         arvoreJuridica.root = arvoreJuridica.inserir(arvoreJuridica.root, cliente);
                     }
-                    /*
-                     * Intancia um objeto da classe Cliente e insere na ABB correspondente
-                     * a tipo de conta
-                     */
                     break;
                 case 2:
                     System.out.print("Qual tipo de conta a oferta se destina ? ");
@@ -87,31 +82,164 @@ public class DivulgaOfertas {
                     } while (op == -1);
                     System.out.print("Qual o valor de saldo mínimo exigido R$: ");
                     saldo = le.nextDouble();
-                    /*
-                     * Fazendo uso de um método da classe ABB, desenvolvido para este
-                     * problema, uma lista de clientes aptos para a oferta é gerada.
-                     * Nesse trecho de programa que tentar fazer o contato com todos os
-                     * clientes presente na lista.
-                     */
+                    List<Cliente> clientes = new ArrayList<>();
                     if (tipoConta.equals("Física")) {
-                        System.out.println("Clientes que aceitaram a oferta");
-                        System.out.println(arvoreFisica.buscar(arvoreFisica.root, saldo));
+                        clientes.addAll(arvoreFisica.buscar(arvoreFisica.root, saldo));
                     } else {
-                        System.out.println("Clientes que aceitaram a oferta");
-                        System.out.println(arvoreJuridica.buscar(arvoreJuridica.root, saldo));
+                        clientes.addAll(arvoreJuridica.buscar(arvoreJuridica.root, saldo));
+                    }
+                    if (clientes.isEmpty()) {
+                        System.out.println("Não há clientes aptos para a oferta");
+                    } else {
+                        while (!clientes.isEmpty()) {
+                            Cliente clienteReceberOferta = clientes.remove(0);
+                            System.out.println("Cliente: " + clienteReceberOferta.getNome());
+                            System.out.println("CPF/CNPJ: " + clienteReceberOferta.getCpfCnpj());
+                            System.out.println("Número da conta: " + clienteReceberOferta.getNumeroConta());
+                            System.out.println("Tipo de conta: " + clienteReceberOferta.getTipoConta());
+                            System.out.println("Saldo: " + clienteReceberOferta.getSaldo());
+                            System.out.println();
+                            System.out.println("Deseja receber a oferta? 1-Sim 2-Não");
+                            op = le.nextInt();
+                            if (op == 1) {
+                                System.out.println("Oferta aceita");
+                                if (tipoConta.equals("Física"))
+                                    arvoreFisica.root = arvoreFisica.remove(arvoreFisica.root, clienteReceberOferta.getSaldo());
+                                else
+                                    arvoreJuridica.root = arvoreJuridica.remove(arvoreJuridica.root, clienteReceberOferta.getSaldo());
+                            } else {
+                                System.out.println("Oferta não aceita");
+                            }
+                        }
                     }
                     break;
                 case 3:
-                    /*
-                     * Implemente o submenu descrito no texto
-                     */
+                    int alt = -1;
+                    while (alt == -1) {
+                        System.out.println(" 1 - Consultar cliente");
+                        System.out.println(" 2 - Alterar saldo");
+                        System.out.println(" 3 - Numero de clientes");
+                        System.out.println(" 4 - Clientes com saldo maior que");
+                        System.out.println(" 5 - voltar ao menu principal");
+                        op = le.nextInt();
+                        switch (op) {
+                            case 1:
+                                System.out.println("Digite se é pessoa 1- física ou 2- jurídica");
+                                op = le.nextInt();
+                                if (op == 1) {
+                                    tipoConta = "Física";
+                                } else {
+                                    tipoConta = "Jurídica";
+                                }
+                                System.out.println("Digite o cpf ou cnpj");
+                                cpfCnpj = le.next();
+                                if (tipoConta.equals("Física")) {
+                                    Cliente clienteProcurado = arvoreFisica.buscarCliente(arvoreFisica.root, cpfCnpj);
+                                    if (clienteProcurado == null) {
+                                        System.out.println("Cliente não encontrado");
+                                    } else {
+                                        System.out.println("Cliente: " + clienteProcurado.getNome());
+                                        System.out.println("CPF: " + clienteProcurado.getCpfCnpj());
+                                        System.out.println("Número da conta: " + clienteProcurado.getNumeroConta());
+                                        System.out.println("Tipo de conta: " + clienteProcurado.getTipoConta());
+                                        System.out.println("Saldo: " + clienteProcurado.getSaldo());
+                                    }
+                                } else {
+                                    Cliente clienteProcurado = arvoreJuridica.buscarCliente(arvoreJuridica.root, cpfCnpj);
+                                    if (clienteProcurado == null) {
+                                        System.out.println("Cliente não encontrado");
+                                    } else {
+                                        System.out.println("Cliente: " + clienteProcurado.getNome());
+                                        System.out.println("CNPJ: " + clienteProcurado.getCpfCnpj());
+                                        System.out.println("Número da conta: " + clienteProcurado.getNumeroConta());
+                                        System.out.println("Tipo de conta: " + clienteProcurado.getTipoConta());
+                                        System.out.println("Saldo: " + clienteProcurado.getSaldo());
+                                    }
+                                }
+                                break;
+                            case 2:
+                                System.out.println("Digite se é pessoa 1- física ou 2- jurídica");
+                                op = le.nextInt();
+                                System.out.println("Digite o numero da conta do cliente");
+                                numeroConta = le.nextInt();
+                                if (op == 1) {
+                                    tipoConta = "Física";
+                                } else {
+                                    tipoConta = "Jurídica";
+                                }
+                                Cliente clienteAlterarSaldo;
+                                if (tipoConta.equals("Física")) {
+                                    clienteAlterarSaldo = arvoreFisica.buscarClienteNumeroConta(arvoreFisica.root, numeroConta);
+                                    if (clienteAlterarSaldo == null) {
+                                        System.out.println("Cliente não encontrado");
+                                    } else {
+                                        arvoreFisica.root = arvoreFisica.remove(arvoreFisica.root, clienteAlterarSaldo.getSaldo());
+                                        System.out.println("Digite o novo saldo");
+                                        saldo = le.nextDouble();
+                                        clienteAlterarSaldo.setSaldo(saldo);
+                                        System.out.println("Saldo alterado com sucesso");
+                                        System.out.println("Cliente: " + clienteAlterarSaldo.getNome());
+                                        System.out.println("CPF: " + clienteAlterarSaldo.getCpfCnpj());
+                                        System.out.println("Número da conta: " + clienteAlterarSaldo.getNumeroConta());
+                                        System.out.println("Tipo de conta: " + clienteAlterarSaldo.getTipoConta());
+                                        System.out.println("Saldo: " + clienteAlterarSaldo.getSaldo());
+                                        System.out.println();
+                                        arvoreFisica.root = arvoreFisica.inserir(arvoreFisica.root, clienteAlterarSaldo);
+                                    }
+                                } else {
+                                    clienteAlterarSaldo = arvoreJuridica.buscarClienteNumeroConta(arvoreJuridica.root, numeroConta);
+                                    if (clienteAlterarSaldo == null) {
+                                        System.out.println("Cliente não encontrado");
+                                    } else {
+                                        arvoreJuridica.root = arvoreJuridica.remove(arvoreJuridica.root, clienteAlterarSaldo.getSaldo());
+                                        System.out.println("Digite o novo saldo");
+                                        saldo = le.nextDouble();
+                                        clienteAlterarSaldo.setSaldo(saldo);
+                                        System.out.println("Saldo alterado com sucesso");
+                                        System.out.println("Cliente: " + clienteAlterarSaldo.getNome());
+                                        System.out.println("CNPJ: " + clienteAlterarSaldo.getCpfCnpj());
+                                        System.out.println("Número da conta: " + clienteAlterarSaldo.getNumeroConta());
+                                        System.out.println("Tipo de conta: " + clienteAlterarSaldo.getTipoConta());
+                                        System.out.println("Saldo: " + clienteAlterarSaldo.getSaldo());
+                                        System.out.println();
+                                        arvoreJuridica.root = arvoreJuridica.inserir(arvoreJuridica.root, clienteAlterarSaldo);
+                                    }
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Numero de clientes: " + arvoreFisica.numeroClientes(arvoreFisica.root));
+                                break;
+                            case 4:
+                                System.out.println("Digite o saldo");
+                                saldo = le.nextDouble();
+                                List<Cliente> clientesComSaldoMaiorQue = arvoreFisica.buscar(arvoreFisica.root, saldo);
+                                if (clientesComSaldoMaiorQue.isEmpty()) {
+                                    System.out.println("Não há clientes com saldo maior que " + saldo);
+                                } else {
+                                    System.out.println("Clientes com saldo maior que " + clientesComSaldoMaiorQue.size());
+                                }
+                                break;
+                            case 5:
+                                alt = 0;
+                                break;
+                        }
+                    }
                     break;
             }
         } while (opcao != 0);
-        System.out.println("Clientes que não aceitaram ou não estavam adequados para a oferta");
-        /*
-         * Esvazia as ABBs apresentando todos os clientes que aguardam nova portunidade
-         */
+        if (arvoreFisica.root != null || arvoreJuridica.root != null) {
+            System.out.println("Clientes que não aceitaram ou não estavam adequados para a oferta");
+            System.out.println();
+
+            arvoreFisica.imprimir(arvoreFisica.root);
+            arvoreJuridica.imprimir(arvoreJuridica.root);
+            arvoreJuridica.root = null;
+            arvoreFisica.root = null;
+        } else {
+            System.out.println("Não há clientes que não aceitaram ou não estavam adequados para a oferta");
+        }
+
+
         le.close();
     }
 }
--- src/cp/abb/entities/ABB.java
+++ src/cp/abb/entities/ABB.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ABB {
+
+
     private class ARVORE {
         Cliente dado;
         ARVORE esq;
         ARVORE dir;
     }
+
     public ARVORE root = null;
 
 
@@ -19,11 +22,10 @@ public class ABB {
             p.dado = info;
             p.esq = null;
             p.dir = null;
-        }
-        else if (info.getSaldo() < p.dado.getSaldo())
-            p.esq= inserir (p.esq, info);
+        } else if (info.getSaldo() < p.dado.getSaldo())
+            p.esq = inserir(p.esq, info);
         else
-            p.dir=inserir(p.dir, info);
+            p.dir = inserir(p.dir, info);
         return p;
     }
 
@@ -40,5 +42,84 @@ public class ABB {
         return clientes;
     }
 
+    public ARVORE remove(ARVORE p, double saldo) {
+        if (p == null) {
+            return null;
+        }
+        if (saldo < p.dado.getSaldo()) {
+            p.esq = remove(p.esq, saldo);
+        } else if (saldo > p.dado.getSaldo()) {
+            p.dir = remove(p.dir, saldo);
+        } else {
+            if (p.esq == null && p.dir == null) {
+                return null;
+            } else if (p.esq == null) {
+                return p.dir;
+            } else if (p.dir == null) {
+                return p.esq;
+            } else {
+                ARVORE aux, ref;
+                aux = p.dir;
+                ref = p.dir;
+                while (aux.esq != null) {
+                    aux = aux.esq;
+                }
+                aux.esq = p.esq;
+                return ref;
+            }
+        }
+        return p;
+    }
 
+    public Cliente buscarCliente(ARVORE root, String cpfCnpj) {
+        if (root == null) {
+            return null;
+        }
+        Cliente cliente;
+        if (root.dado.getCpfCnpj().equals(cpfCnpj)) {
+            return root.dado;
+        }
+        cliente = buscarCliente(root.dir, cpfCnpj);
+        if (cliente != null) {
+            return cliente;
+        }
+        cliente = buscarCliente(root.esq, cpfCnpj);
+        return cliente;
+    }
+
+    public Cliente buscarClienteNumeroConta(ARVORE root, int numeroConta) {
+        if (root == null) {
+            return null;
+        }
+        Cliente cliente;
+        if (root.dado.getNumeroConta() == (numeroConta)) {
+            return root.dado;
+        }
+        cliente = buscarClienteNumeroConta(root.dir, numeroConta);
+        if (cliente != null) {
+            return cliente;
+        }
+        cliente = buscarClienteNumeroConta(root.esq, numeroConta);
+        return cliente;
+    }
+
+    public int numeroClientes(ARVORE root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + numeroClientes(root.esq) + numeroClientes(root.dir);
+    }
+
+    public void imprimir(ARVORE p) {
+        if (p != null) {
+            imprimir(p.dir);
+            System.out.println("Cliente: " + p.dado.getNome());
+            System.out.println("CPF/CNPJ: " + p.dado.getCpfCnpj());
+            System.out.println("Número da conta: " + p.dado.getNumeroConta());
+            System.out.println("Tipo de conta: " + p.dado.getTipoConta());
+            System.out.println("Saldo: " + p.dado.getSaldo());
+            System.out.println();
+            imprimir(p.esq);
+        }
+    }
 }
